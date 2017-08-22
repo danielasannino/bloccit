5c1321a79940a28ce9a0183299cec85d2c5dd0f1
Daniela Sannino <danielasannino@Danielas-MacBook-Pro.local>
2017-08-21 21:28:23 -0400

    Update routes again

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'welcome#contact'
 
-  get 'welcome/faq'
-
-  get 'welcome/contact'
-
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
