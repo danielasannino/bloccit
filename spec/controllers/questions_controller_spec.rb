90338be341fe4310fe356cbd12470c23a343304d
Daniela Sannino <danielasannino@Danielas-MacBook-Pro.local>
2017-09-07 19:08:41 -0400

    updated GET new test

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -43,12 +43,12 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe "GET #new" do
     it "returns http success" do
-      get :new, params: {id: my_question.id}
+      get :new
       expect(response).to have_http_status(:success)
     end
 
     it "renders the #new view" do
-      get :new, params: {id: my_question.id}
+      get :new
       expect(response).to render_template :new
     end
 
