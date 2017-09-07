bd0992ba148782be2bfeb6aed934288d9bc72acb
Daniela Sannino <danielasannino@Danielas-MacBook-Pro.local>
2017-09-07 18:54:45 -0400

    created questions controller & spec tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   resources :advertisements
 
   resources :posts
+
+  resources :questions
 
   get 'about' => 'welcome#about'
 
---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  include RandomData
+
+  let (:my_question) do
+    Question.create(
+    id: 1,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: false
+    )
+  end
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns [my_question] to @questions" do
+      get :index
+      expect(assigns(:questions)).to eq([my_question])
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, params: {id: my_question.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, params: {id: my_question.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_question to @question" do
+      get :show, params: {id: my_question.id}
+      expect(assigns(:question)).to eq(my_question)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new, params: {id: my_question.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new, params: {id: my_question.id}
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @question" do
+      get :new
+      expect(assigns(:question)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+    it "increases the number of questions by 1" do
+      expect{ post :create, params: { question: {title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: false}}}.to change(Question,:count).by(1)
+    end
+
+    it "assigns the new question to @question" do
+      post :create, params: { question: {title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: false} }
+      expect(assigns(:question)).to eq Question.last
+    end
+
+    it "redirects to the new question" do
+      post :create, params: { question: {title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: false} }
+      expect(response).to redirect_to Question.last
+    end
+  end
+end
