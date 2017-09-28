c2b467065734ef271437508ea10ca5d27f783bfd
Daniela Sannino <danielasannino@Danielas-MacBook-Pro.local>
2017-09-27 20:20:58 -0400

    created private methods & fixed params error in user_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase if email.present? }
+  before_save :downcase_email, if: :email_present?
   before_save :format_name
 
   validates :name, length:  { minimum: 1, maximum: 100 }, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_digest_nil?
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email,
             presence: true,
@@ -12,13 +12,27 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  private
+
+  def downcase_email
+    email.downcase
+  end
+
   def format_name
-    if name
+    if name.present?
       name_array = []
       name.split.each do |name_part|
         name_array << name_part[0].upcase + name_part[1..-1]
       end
-      self.name = name_array.join(" ")
+      name = name_array.join(" ")
     end
+  end
+
+  def password_digest_nil?
+    password_digest.nil?
+  end
+
+  def email_present?
+    email.present?
   end
 end
