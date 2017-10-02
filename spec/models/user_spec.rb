65e621d4071868ac455ebd907211ddb067ec4579
Daniela Sannino <danielasannino@Danielas-MacBook-Pro.local>
2017-10-02 19:09:43 -0400

    assignment 27

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,87 +1,95 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+    let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
 
-  it { is_expected.to have_many(:posts) }
+    it { should have_many(:posts)}
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(1) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_length_of(:email).is_at_least(3) }
+    it { should allow_value("user@bloccit.com").for(:email) }
+    it { should_not allow_value("userbloccit.com").for(:email) }
 
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_length_of(:name).is_at_least(1) }
-
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_uniqueness_of(:email) }
-  it { is_expected.to validate_length_of(:email).is_at_least(3) }
-  it { is_expected.to allow_value("user@bloccit.com").for(:email) }
-
-  it { is_expected.to validate_presence_of(:password) }
-  it { is_expected.to have_secure_password }
-  it { is_expected.to validate_length_of(:password).is_at_least(6) }
+    it { should validate_presence_of(:password) }
+    it { should have_secure_password }
+    it { should validate_length_of(:password).is_at_least(6) }
 
   describe "attributes" do
-    it "should have name and email attributes" do
-      expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+    it "should respond to name" do
+      expect(user).to respond_to(:name)
+    end
+    it "should respond to email" do
+      expect(user).to respond_to(:email)
     end
 
-    it "should format the user's name" do
-      user.name = "bloccit user"
-      user.save
-      expect(user.name).to eq("Bloccit User")
-    end
-
-    it "responds to role" do
+    it "should respond to role" do
       expect(user).to respond_to(:role)
     end
-
-    it "responds to admin?" do
+    it "should respond to admin?" do
       expect(user).to respond_to(:admin?)
     end
-
-    it "responds to member?" do
+    it "should respond to member?" do
       expect(user).to respond_to(:member?)
+    end
+    it "should respond to moderator?" do
+      expect(user).to respond_to(:moderator?)
+    end
+  end
+
+  describe "invalid user" do
+    let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
+    let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+    let(:user_with_invalid_email_format) { User.new(name: "Bloccit User", email: "invalid_format") }
+
+    it "should be an invalid user due to blank name" do
+      expect(user_with_invalid_name).to_not be_valid
+    end
+    it "should be an invalid user due to blank email" do
+      expect(user_with_invalid_email).to_not be_valid
+    end
+    it "should be an invalid user due to incorrectly formatted email address" do
+      expect(user_with_invalid_email_format).to_not be_valid
     end
   end
 
   describe "roles" do
-    it "is member by default" do
+    it "should be member by default" do
       expect(user.role).to eql("member")
     end
-
-    context "member user" do
-      it "returns true for #member?" do
-        expect(user.member?).to be_truthy
-      end
-
-      it "returns false for #admin?" do
-        expect(user.admin?).to be_falsey
-      end
+  context "member user" do
+    it "should return true for #member?" do
+      expect(user.member?).to be_truthy
     end
-
-    context "admin user" do
+    it "should return false for #admin?" do
+      expect(user.admin?).to be_falsey
+    end
+  end
+  context "admin user" do
       before do
         user.admin!
       end
-
-      it "returns false for #member?" do
+      it "should return false for #member?" do
         expect(user.member?).to be_falsey
       end
-
-      it "returns true for #admin?" do
+      it "should return true for #admin?" do
         expect(user.admin?).to be_truthy
       end
     end
+  context "moderator user" do
+      before do
+        user.moderator!
+      end
+      it "should return false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+      it "should return false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+      it "should return true for #admin?" do
+        expect(user.moderator?).to be_truthy
+      end
+    end
   end
-end
-
-describe "invalid user" do
-  let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-  let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
-
-  it "should be an invalid user due to blank name" do
-    expect(user_with_invalid_name).to_not be_valid
-  end
-
-  it "should be an invalid user due to blank email" do
-    expect(user_with_invalid_email).to_not be_valid
-  end
- end
 end
