eede0cb113570ef814ce894710d7795a68dc35f8
Daniela Sannino <danielasannino@Danielas-MacBook-Pro.local>
2017-10-09 18:27:32 -0400

    checkpoint 29

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
   before_save :downcase_email, if: :email_present?
   before_save { self.role ||= :member }
--- db/seeds.rb
+++ db/seeds.rb
@@ -31,12 +31,16 @@ end
 
  50.times do
 
-   Post.create!(
+   post = Post.create!(
      user:   users.sample,
      topic: topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
+
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
  end
  posts = Post.all
 
@@ -81,4 +85,5 @@ member = User.create!(
  puts "#{SponsoredPost.count} sponsored posts created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Vote.count} votes created"
  puts "#{Question.count} questions created"
